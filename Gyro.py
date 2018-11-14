44f9c1cc37c2eaabe30b4e9465ef9398973f2203
Sander Rask Finnemann <sand4384@edu.easj.dk>
2018-11-14 11:35:49 +0100

    Gyro

---
+++ Gyro.py
@@ -0,0 +1,20 @@
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+red = (255,0,0)
+
+while true:
+    acc = sense.get_accelerometer_raw()
+    x = acc['x']
+    y = acc['y']
+    z = acc['z']
+
+    x = abs(x)
+    y = abs(y)
+    z = abs(z)
+
+    if x > 1 or y > 1 or z > 1:
+        sense.show_letter("!", red)
+    else:
+        sense.clear() 
