6083577810ace73a5f4f7436bdc9a45b9f51656d
Sander Rask Finnemann <sand4384@edu.easj.dk>
2018-11-14 10:36:54 +0100

    Thread

--- Rasp.py
+++ Rasp.py
@@ -1,5 +1,6 @@
 from sense_hat import SenseHat
 import time
+import threading
 
 sense = SenseHat()
 sense.clear()
@@ -18,34 +19,38 @@ i = 0
 
 c = white
 
-while(l<10):
-  print("run: " + str(l+1))
-  while(i<24):
+#Thread 1
+def tone():
+  while(l<10):
+    print("run: " + str(l+1))
+    while(i<24):
+      time.sleep(0.05)
+      sense.set_pixel(x,y,c)
+      if x < 7 and y == 0:
+        x += 1
+      if x == 7 and y < 7:
+        y += 1
+      if y == 7 and x > 0:
+        x -= 1
+      if x == 0 and y > 0:
+        y -= 1
+      i += 1
     time.sleep(0.05)
-    sense.set_pixel(x,y,c)
-    if x < 7 and y == 0:
-      x += 1
-    if x == 7 and y < 7:
-      y += 1
-    if y == 7 and x > 0:
-      x -= 1
-    if x == 0 and y > 0:
-      y -= 1
-    i += 1
-  time.sleep(0.05)
-  l += 1
-  i = 0
-  x = 1
-  y = 0
-  if c == red:
-    c = white
-  if c == green:
-    c = red
-  if c == blue:
-    c = green
-  if c==white:
-    c = blue
-  sense.clear()
+    l += 1
+    i = 0
+    x = 1
+    y = 0
+    if c == red:
+      c = white
+    if c == green:
+      c = red
+    if c == blue:
+      c = green
+    if c==white:
+      c = blue
+    sense.clear()
+
+threading.Thread(target=tone).start()
 
 print("done")
 time.sleep(5)
