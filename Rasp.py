e5159c8a14e2f7dbf02d0064fd7a36de05df40af
Sander Rask Finnemann <sand4384@edu.easj.dk>
2018-11-14 11:01:45 +0100

    second thread

--- Rasp.py
+++ Rasp.py
@@ -14,11 +14,14 @@ blue = (0,0,255)
 x = 1
 y = 0
 
+z = 2
+x = 1
+
 l = 0
 i = 0
 
 c = white
-
+q = blue
 #Thread 1
 def tone(l,i,x,y,c):
   while(l<10):
@@ -50,8 +53,40 @@ def tone(l,i,x,y,c):
       c = blue
     sense.clear()
 
-threading.Thread(target=tone(l,i,x,y,c)).start()
+#Thread 2
+def ttwo(l,i,x,y,c)
+  while(l<10):
+    print("run: " + str(l+1))
+    while(i<16):
+      time.sleep(0.05)
+      sense.set_pixel(x,y,c)
+      if x < 5 and y == 1:
+        x += 1
+      if x == 6 and y < 6:
+        y += 1
+      if y == 6 and x > 1:
+        x -= 1
+      if x == 1 and y > 1:
+        y -= 1
+      i += 1
+    time.sleep(0.05)
+    l += 1
+    i = 0
+    x = 1
+    y = 0
+    if c == red:
+      c = white
+    if c == green:
+      c = red
+    if c == blue:
+      c = green
+    if c==white:
+      c = blue
+    sense.clear()
 
+
+threading.Thread(target=tone(l,i,x,y,c)).start()
+threading.Thread(target=ttwo(l,i,z,v,q)).start()
 print("done")
 time.sleep(5)
 sense.clear()
