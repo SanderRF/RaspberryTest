f21b8fb5c4b58ec92d315f6718c7efc6f0e73c3f
Sander <33893015+SanderRF@users.noreply.github.com>
2018-11-14 09:47:32 +0100

    Create Rasp.py

---
+++ Rasp.py
@@ -0,0 +1,35 @@
+from sense_hat import SenseHat
+import time
+
+sense = SenseHat()
+
+c = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+
+x = 1
+y = 0
+
+l = 0
+i = 0
+
+c = white
+
+while(l<2):
+  while(i<24):
+    time.sleep(0.05)
+    sense.set_pixel(x,y,c)
+    if x < 7 and y == 0:
+      x += 1
+    if x == 7 and y < 7:
+      y += 1
+    if y == 7 and x > 0:
+      x -= 1
+    if x == 0 and y > 0:
+      y -= 1
+    i += 1
+  
+  l += 1
+  
