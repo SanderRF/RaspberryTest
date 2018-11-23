5809ac0c4e2ea536c22d1a96e69f7896e33b7203
Sander Rask Finnemann <sand4384@edu.easj.dk>
2018-11-23 10:49:58 +0100

    cancer

--- BroadcastPie/Broad.py
+++ BroadcastPie/Broad.py
@@ -9,20 +9,10 @@ g = (0,100,0)
 b = (0,0,100)
 w = (0,0,0)
 r = (100,0,0)
-image = [
-w,w,w,w,w,w,w,w,
-w,w,w,g,g,w,w,w,
-w,w,w,g,g,w,w,w,
-w,r,r,r,r,r,r,w,
-w,w,w,r,r,w,w,w,
-w,w,b,b,b,b,w,w,
-w,w,b,w,w,b,w,w,
-w,w,w,w,w,w,w,w
-]
 
-sense.set_pixels(image)
-time.sleep(1)
-sense.clear()
-sense.show_message("Jeg vil spille bold!////////////" , text_colour=g, back_colour=b, scroll_speed=0.1)
+temp = sense.get_temperature()
+print(temp)
+
+sense.show_message(str(temp)
 
 sense.clear()
