42bc1e8fd93fc42b2157c72192a9e61abb296743
Sander Rask Finnemann <sand4384@edu.easj.dk>
2018-11-23 12:22:39 +0100

    cancer

--- BroadcastPie/Broad.py
+++ BroadcastPie/Broad.py
@@ -6,14 +6,14 @@ from datetime import datetime
 sense = SenseHat()
 sense.clear()
 
-temp = sense.get_temperature()
+temp = sense.get_temperature
 
 s = socket(AF_INET, SOCK_DGRAM)
 #s.bind(('', 6969))  #(ip,port)
 
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 while True:
-    data = "Current temperature " + str(temp)
-    s.sendto(bytes(data), ('<broadcast>', 6969))
+    data = "Current time " + str(datetime.now())
+    s.sendto(bytes(data,"UTF-8"), ('<broadcast>', 6969))
     print(data)
     time.sleep(1)
