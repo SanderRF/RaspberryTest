bb9403b5a00fbd8835fcffc3af45c4d431d61816
Sander Rask Finnemann <sand4384@edu.easj.dk>
2018-11-23 11:44:05 +0100

    cancer

--- BroadcastPie/Broad.py
+++ BroadcastPie/Broad.py
@@ -8,7 +8,7 @@ s = socket(AF_INET, SOCK_DGRAM)
 
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 while True:
-    data = "Current time" + str(datetime.now())
+    data = "Current time " + str(datetime.now())
     s.sendto(bytes(data,"UTF-8"), ('<broadcast>', 6969))
     print(data)
     time.sleep(1)
