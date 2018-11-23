092b8c0883fd1ae9309c1a1525feb545c6e70730
Sander Rask Finnemann <sand4384@edu.easj.dk>
2018-11-23 12:21:42 +0100

    cancer

--- BroadcastPie/Broad.py
+++ BroadcastPie/Broad.py
@@ -13,7 +13,7 @@ s = socket(AF_INET, SOCK_DGRAM)
 
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 while True:
-    data = "Current temperature " + temp
+    data = "Current temperature " + str(temp)
     s.sendto(bytes(data), ('<broadcast>', 6969))
     print(data)
     time.sleep(1)
